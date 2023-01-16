2678419f0bb9c48db772c4590bccdada190449bd
Ewelina Nowak <ewelinanowak@Ewelinas-MacBook-Pro.local>
2023-01-16 16:35:04 +0100

    fixed dark mode

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -13,7 +13,7 @@ export default async function SearchPage({ searchParams }: Props) {
   );
   return (
     <div>
-      <h1 className="underline decoration-blue-500">
+      <h1 className="dark:text-slate-100 underline decoration-blue-500">
         Search results for: {searchParams?.term}
       </h1>
       <NewsList news={news} />
