7e795ff485bf89846f620865303223bbf2292f84
Ewelina Nowak <ewelinanowak@Ewelinas-MacBook-Pro.local>
2023-01-16 11:11:52 +0100

    finished data fetching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "680px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+    },
   },
   plugins: [],
-}
+};
